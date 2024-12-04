e9641cb263387cc33ad12f8a085ebbfd3d544ec8
Louisa Whitton <nowlouisa@gmail.com>
2024-12-04 18:38:06 +0000

    feat: Add header with image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
+import { HeaderComponent } from './layout/header/header.component';
+// import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
