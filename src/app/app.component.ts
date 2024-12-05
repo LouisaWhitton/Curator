cb85483fa3406cb195dd196b62033792f2310801
Louisa Whitton <nowlouisa@gmail.com>
2024-12-05 11:54:29 +0000

    style: Further styling of header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
-// import { FooterComponent } from "./layout/footer/footer.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
