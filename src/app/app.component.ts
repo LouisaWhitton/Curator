c1ed0f812479638bd29bc224b9968b3948b6a64c
Louisa Whitton <nowlouisa@gmail.com>
2024-12-07 13:37:34 +0000

    build: Set up for dotenv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from "./layout/footer/footer.component";
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Curator';
+
+  ngOnInit() {
+    if (isDevMode()) {
+      console.log('Development!');
+    } else {
+      console.log('Production!');
+    }
+
+    let myString: string = process.env["API_KEY"] || 'DEF_A';
+    console.log(myString);
+  }
+
 }
