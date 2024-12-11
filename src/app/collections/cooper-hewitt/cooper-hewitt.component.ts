f15b425e15efa28032cc30b51865546554758df8
Louisa Whitton <nowlouisa@gmail.com>
2024-12-11 15:44:39 +0000

    feat: generate components for 3 collections

---
+++ src/app/collections/cooper-hewitt/cooper-hewitt.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cooper-hewitt',
+  imports: [],
+  templateUrl: './cooper-hewitt.component.html',
+  styleUrl: './cooper-hewitt.component.scss'
+})
+export class CooperHewittComponent {
+
+}
---
+++ src/app/collections/rijksmuseum/rijksmuseum.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rijksmuseum',
+  imports: [],
+  templateUrl: './rijksmuseum.component.html',
+  styleUrl: './rijksmuseum.component.scss'
+})
+export class RijksmuseumComponent {
+
+}
