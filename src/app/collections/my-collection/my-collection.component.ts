47c1196ad5c450971a6b888877ef999bcada1118
Louisa Whitton <nowlouisa@gmail.com>
2025-01-05 20:08:12 +0000

    feat: add link to Home Page from other components

--- src/app/collections/my-collection/my-collection.component.ts
+++ src/app/collections/my-collection/my-collection.component.ts
@@ -4,11 +4,12 @@ import { Fieldset } from 'primeng/fieldset';
 import { FloatLabel } from 'primeng/floatlabel';
 import { Button } from 'primeng/button';
 import { ListItemsComponent } from './list-items/list-items.component';
+import {RouterOutlet, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-my-collection',
   standalone: true,
-  imports: [FormsModule, ListItemsComponent, ReactiveFormsModule],
+  imports: [FormsModule, ListItemsComponent, ReactiveFormsModule, RouterLink, RouterOutlet],
   templateUrl: './my-collection.component.html',
   styleUrl: './my-collection.component.scss'
 })
